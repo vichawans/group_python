02a7637d330bae551f6607436364ef7d77b28414
vichawans <vs480@cam.ac.uk>
2025-06-10 22:29:36 +0100

    Add slurm sbatch with mas qos

--- bin/download_pp_convert/u-xxnnn/batch_retrieve_stash.sh
+++ bin/download_pp_convert/u-xxnnn/batch_retrieve_stash.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#SBATCH --job-name=u-dm931
+#SBATCH --account=mass
+#SBATCH --partition=mass # standard or debug
+#SBATCH --qos=mass
+#SBATCH -o %j.out
+#SBATCH -e %j.err
+#SBATCH --time=24:00:00
+#SBATCH --array=26-32 # edit to match the range in array_stream_stash.txt. This can select only the lines needed
+#SBATCH --mem=200000
 
 # batch download pp files using retrieve_stash.sh
 
