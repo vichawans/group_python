17f488b0d27581c43ffe153e1b70bd2a8291c5a3
vichawans <vs480@cam.ac.uk>
2025-08-11 22:27:30 +0100

    Edit retrieve_pp.sh

--- scripts/data_extraction/download_pp_convert/batch_retrieve_pp.sh
+++ scripts/data_extraction/download_pp_convert/batch_retrieve_pp.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
-#SBATCH --job-name=u-dm931
-#SBATCH --account=mass
-#SBATCH --partition=mass # standard or debug
-#SBATCH --qos=mass
+#SBATCH --job-name=query-control
+#SBATCH --account=acsis
+#SBATCH --partition=standard # standard or debug
+#SBATCH --qos=standard
 #SBATCH -o %j.out
 #SBATCH -e %j.err
 #SBATCH --time=24:00:00
-#SBATCH --array=26-32 # edit to match the range in array_stream_stash.txt. This can select only the lines needed
+#SBATCH --array=1-5 # edit to match the range in array_stream_stash.txt. This can select only the lines needed
 #SBATCH --mem=200000
 
-# batch download pp files using retrieve_stash.sh
+# driver script to batch download pp files using retrieve_stash.sh
 
 # Usage: sh retrieve_pp_batch.sh
 
-# EDIT THE JOB ID AND FOLDER NAME -- use scratch space for speed
-jobID="u-bl551"
-tmpdir="/work/scratch-pw2/vs480/pp_files/$jobID"
+# Set config
+tmpdir="/work/scratch-pw2/vs480/"
+imax=3
+queries=""
+processing_queue='./processing_queue.csv'
+SLURM_ARRAY_TASK_ID=1
 
-pp_dir="$tmpdir/pp_files"
+
+# Extract the stream and stash for the current $SLURM_ARRAY_TASK_ID
+jobID=$(awk -F, -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $processing_queue)
+stream=$(awk -F, -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $3}' $processing_queue)
+stash=$(awk -F, -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $4}' $processing_queue)
 
 
-# Lines below loop over the years and download the data. 
-# Edit the stash number and stream name as needed.
-# TODO: read array_stream_stash.txt file to get the stream and stash numbers
-stream="apm.pp"; stash=34008; sh retrieve_stash.sh $jobID $stream $stash "$pp_dir/${jobID}_${stream}_${stash}" $imax  $>> retrieve_pp_${stash}.log & # H2O2 mmr
+# Internal variables
+pp_dir="$tmpdir/pp_files/${jobID}_${stream}_${stash}"
+
+# create pp_dir
+
+# sh retrieve_stash.sh $jobID $stream $stash $pp_dir $imax $queries $>> retrieve_pp_${stash}.log & 
