1a931d7ded9cfbfcf0b0b848b5fde4cf81bdd133
vichawans <vs480@cam.ac.uk>
2025-08-19 20:24:39 +0100

    Edit to add account, otherwise will not work

--- scripts/data_extraction/download_pp_convert/batch_retrieve_pp.sh
+++ scripts/data_extraction/download_pp_convert/batch_retrieve_pp.sh
@@ -6,7 +6,7 @@
 #SBATCH -o %j.out
 #SBATCH -e %j.err
 #SBATCH --time=1:00:00
-#SBATCH --array=1-25 # edit to match the range in array_stream_stash.txt. This can select only the lines needed
+#SBATCH --array=1-24 # edit to match the range in array_stream_stash.txt. This can select only the lines needed
 #SBATCH --mem=200000
 
 # driver script to batch download pp files using retrieve_stash.sh
@@ -16,7 +16,7 @@
 # Set config
 # TODO: change to config.yaml
 tmpdir="/work/scratch-pw2/vs480"
-processing_queue='./processing_queue_u-dq721.csv'
+processing_queue='./processing_queue_extra-noy.csv'
 
 # Get current task ID, either from slurm or from passing in the variable from interactive shell or from reading config file
 TASK_ID=$SLURM_ARRAY_TASK_ID
@@ -45,6 +45,7 @@ echo "Created/verified directory: $download_dir"
 SLURM_RETRIEVE_ID=$(sbatch --parsable \
        --job-name="$jobID $stash pp" \
        --partition="mass" \
+       --account="mass" \
        --qos="mass" \
        retrieve_stash.sh "$jobID" "$stream" "$stash" "$download_dir")
 
