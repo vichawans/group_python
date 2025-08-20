2b9849d93ddc0cf42918b6ca3ad1079b680d0315
vichawans <vs480@cam.ac.uk>
2025-08-20 18:50:45 +0100

    add const for later reference

---
+++ src/const.py
@@ -0,0 +1,22 @@
+# Earth's constants
+R_EARTH =  6.3781e6 # metres
+S2YR = 3600 * 24 * 360  # number of seconds in 1 year
+
+# Physical and chemical constants
+N_A = 6.02214076e23 # Avogadro number (molecule / mole)
+K_BOLTZMANN = 1.380649e-23 # Boltzmann constant (J K-1)
+
+# chemical molar mass
+M_AIR = 28.97e-3 # kg/mol = 28.97 g/mol air
+M_SO2 = 64.066e-3 # kg/mol
+M_OH = 17.008e-3
+M_O3 = 48e-3
+M_H2O2 = 34.0147e-3
+M_S = 32.065e-3
+M_SO4 = 96.06e-3
+M_DMS = 62.13e-3
+M_H2SO4 = 98.079e-3
+M_NO = 30.01e-3
+M_NO2 = 46.055e-3
+M_NO3 = 62.0049e-3
+M_HONO2 = 47.013e-3
