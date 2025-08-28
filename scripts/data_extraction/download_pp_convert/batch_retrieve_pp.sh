6b49452b63cf8e4d15b5a36dc44e4a2cbfd545b2
vichawans <vs480@cam.ac.uk>
2025-08-28 14:50:32 +0100

    download cmip7 sad suite data

--- scripts/data_extraction/download_pp_convert/batch_retrieve_pp.sh
+++ scripts/data_extraction/download_pp_convert/batch_retrieve_pp.sh
@@ -6,7 +6,7 @@
 #SBATCH -o %j.out
 #SBATCH -e %j.err
 #SBATCH --time=1:00:00
-#SBATCH --array=1,4,7,10,13,16,19,22,25-40 # edit to match the range in array_stream_stash.txt. This can select only the lines needed
+#SBATCH --array=1-4 # edit to match the range in array_stream_stash.txt. This can select only the lines needed
 #SBATCH --mem=200000
 
 # driver script to batch download pp files using retrieve_stash.sh
@@ -16,7 +16,7 @@
 # Set config
 # TODO: change to config.yaml
 tmpdir="/work/scratch-pw2/vs480"
-processing_queue='./processing_queue_extra-noy.csv'
+processing_queue='./processing_queue_cmip7_sad.csv'
 
 # Get current task ID, either from slurm or from passing in the variable from interactive shell or from reading config file
 TASK_ID=$SLURM_ARRAY_TASK_ID
